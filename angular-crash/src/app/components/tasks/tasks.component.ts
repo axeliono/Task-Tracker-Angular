cf18f89e724dfa8153a595afc7044841fe87c8d2
axeliono <69375446+axeliono@users.noreply.github.com>
2023-05-02 07:42:37 -0500

    added task item, tasks, button, header components

---
+++ angular-crash/src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Task } from 'src/app/Models/Task';
+import { TASKS } from 'src/app/mock-tasks';
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent {
+  tasks: Task[] = TASKS;
+}
---
+++ angular-crash/src/app/services/task.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Task } from '../Models/Task';
+import { TASKS } from '../mock-tasks';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor() { }
+
+  
+}
